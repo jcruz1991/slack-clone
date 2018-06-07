1c3ba1e3259d7d9865e568d82c347bb3022b50d6
Julio Cruz <atlascruz009@gmail.com>
2018-06-07 13:33:32 -0700

    Added sidebare container. Moved teams and channels components to sidebar and connected using graphql

--- slack-clone-client/src/routes/ViewTeam.js
+++ slack-clone-client/src/routes/ViewTeam.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
-
-import Channels from '../components/Channels';
 import AppLayout from '../components/AppLayout';
 import Header from '../components/Header';
 import SendInput from '../components/SendInput';
 import Messages from '../components/Messages';
-import Teams from '../components/Teams';
+import Sidebar from '../containers/Sidebar';
 
 
 class ViewTeam extends Component {
     render() {
         return(
             <AppLayout>
-                <Teams
-                  teams={[{id: 1, letter:"T"}, {id: 2, letter:"B"}]}
-                />
-                <Channels 
-                  teamName='Team Name'  
-                  username='Username' c
-                  channels={[{id: 1, name:"general"}, {id: 2, name:"random"}]}
-                  users={[{id: 1, name:"slackbot"}, {id: 2, name:"user1"}]}
-                />
+                <Sidebar currentTeamId={1} />
                 <Header
                  channelName="general"
                 />
